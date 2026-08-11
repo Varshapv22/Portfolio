a03d82ecd494321aba4b120305367c88f37f2cae
varsha p v <123231414+Varshapv22@users.noreply.github.com>
2026-08-11 12:43:45 +0530

    Set base path for portfolio in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Portfolio/',
+
   build: {
     rollupOptions: {
       input: {
-        main:    'index.html',
+        main: 'index.html',
         journey: 'journey.html',
-        boat:    'boat_game.html',
+        boat: 'boat_game.html',
       },
     },
   },
